fbb1e4827fe83c3632e8ef2dae9cc3c601b416d8
Allan Ritchie <allan.ritchie@gmail.com>
2015-05-07 21:17:48 -0400

    Void implementation

---
+++ Acr.Biometrics/VoidBiometricImpl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace Acr.Biometrics {
+
+    public class VoidBiometricImpl : IBiometrics {
+
+        public bool IsAvailable {
+            get { return false; }
+        }
+
+
+        public async Task<bool> Evaluate(string message) {
+            return false;
+        }
+    }
+}
